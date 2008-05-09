285db9bf9db1fe2ca7b7cf8f10c4d64ef3cbdf5e
Arve Hjønnevåg <arve@android.com>
2008-05-08 21:04:16 -0700

    Fix suspend_console/resume_console use only one semaphore.
    This fixes a race if another thread acquired the console
    while it was suspended and released it after it was resumed
    the secondary console semaphre would be left locked, and the
    primary semaphore would be released twice. This in turn caused
    the console switch on suspend or resume to hang forever.
    
    Note that suspend_console does not actually lock the console
    for clients that use acquire_console_sem, it only locks it for
    clients that use try_acquire_console_sem. If we change
    suspend_console to fully lock the console, then the kernel
    may deadlock on suspend.
    
    Signed-off-by: Brian Swetland <swetland@google.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -73,7 +73,6 @@ EXPORT_SYMBOL(oops_in_progress);
  * driver system.
  */
 static DECLARE_MUTEX(console_sem);
-static DECLARE_MUTEX(secondary_console_sem);
 struct console *console_drivers;
 /*
  * This is used for debugging the mess that is the VT code by
@@ -926,12 +925,14 @@ void suspend_console(void)
 	printk("Suspending console(s)\n");
 	acquire_console_sem();
 	console_suspended = 1;
+	up(&console_sem);
 }
 
 void resume_console(void)
 {
 	if (!console_suspend_enabled)
 		return;
+	down(&console_sem);
 	console_suspended = 0;
 	release_console_sem();
 }
@@ -947,11 +948,9 @@ void resume_console(void)
 void acquire_console_sem(void)
 {
 	BUG_ON(in_interrupt());
-	if (console_suspended) {
-		down(&secondary_console_sem);
-		return;
-	}
 	down(&console_sem);
+	if (console_suspended)
+		return;
 	console_locked = 1;
 	console_may_schedule = 1;
 }
@@ -961,6 +960,10 @@ int try_acquire_console_sem(void)
 {
 	if (down_trylock(&console_sem))
 		return -1;
+	if (console_suspended) {
+		up(&console_sem);
+		return -1;
+	}
 	console_locked = 1;
 	console_may_schedule = 0;
 	return 0;
@@ -999,7 +1002,7 @@ void release_console_sem(void)
 	unsigned wake_klogd = 0;
 
 	if (console_suspended) {
-		up(&secondary_console_sem);
+		up(&console_sem);
 		return;
 	}
 
