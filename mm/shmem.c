795b2e6a2dca7caceb9e3fb89c5daf689ddd803a
Robert Love <rlove@google.com>
2008-10-14 10:00:47 -0400

    ashmem for 2.6.27.
    Forward port of ashmem to 2.6.27.
    
    Signed-off-by: Robert Love <rlove@google.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2583,6 +2583,14 @@ put_memory:
 	return ERR_PTR(error);
 }
 
+void shmem_set_file(struct vm_area_struct *vma, struct file *file)
+{
+	if (vma->vm_file)
+		fput(vma->vm_file);
+	vma->vm_file = file;
+	vma->vm_ops = &shmem_vm_ops;
+}
+
 /**
  * shmem_zero_setup - setup a shared anonymous mapping
  * @vma: the vma to be mmapped is prepared by do_mmap_pgoff
@@ -2595,10 +2603,7 @@ int shmem_zero_setup(struct vm_area_struct *vma)
 	file = shmem_file_setup("dev/zero", size, vma->vm_flags);
 	if (IS_ERR(file))
 		return PTR_ERR(file);
+	shmem_set_file(vma, file);
 
-	if (vma->vm_file)
-		fput(vma->vm_file);
-	vma->vm_file = file;
-	vma->vm_ops = &shmem_vm_ops;
 	return 0;
 }
