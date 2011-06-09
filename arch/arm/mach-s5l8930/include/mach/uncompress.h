6514230e4295cd5b9c0db2fea27f6e5831313ad7
Ricky Taylor <rickytaylor26@gmail.com>
2011-06-09 10:22:16 +0100

    S5L8930: Added debug UART support during zImage decompression.

--- arch/arm/mach-s5l8930/include/mach/uncompress.h
+++ arch/arm/mach-s5l8930/include/mach/uncompress.h
@@ -1,9 +1,18 @@
+#define UART_UTRSTAT (*(volatile unsigned char*)(0x82500010))
+#define UART_UTXH (*(volatile unsigned char*)(0x82500020))
+
 static inline void putc(int c)
 {
+	while((UART_UTRSTAT & 4) == 0)
+		barrier();
+
+	UART_UTXH = c;
 }
 
 static inline void flush(void)
 {
+	while((UART_UTRSTAT & 4) == 0)
+		barrier();
 }
 
 /*
