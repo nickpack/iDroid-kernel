2a566bbb9b9221c9b1cdae38efd62859e52237f1
Ricky Taylor <rickytaylor26@gmail.com>
2011-09-13 18:41:14 +0100

    S5L8930: Added missing clock header.

---
+++ arch/arm/mach-s5l8930/include/mach/clock.h
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2011 Richard Ian Taylor.
+ *
+ * This file is part of the iDroid Project. (http://www.idroidproject.org).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef  __S5L8930_CLOCK__
+#define  __S5L8930_CLOCK__
+
+extern void s5l_clock_gate_reset(struct clk *_clk);
+
+#endif //__S5L8930_CLOCK__
